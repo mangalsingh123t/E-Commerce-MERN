4d2049880b84764d7a28962debf329b51cd10c54
mangalsingh123t <121483110+mangalsingh123t@users.noreply.github.com>
2024-07-27 11:20:24 +0530

    Add Product Description

---
+++ src/componants/descriptionBox/DescriptionBox.jsx
@@ -0,0 +1,16 @@
+
+export const DescriptionBox = () => {
+    return (
+        <>
+            <div className="mt-20">
+                <div className="flex">
+                    <div className="p-2 bg-slate-50 border">Description</div>
+                    <div className="p-2 bg-slate-50 border">Reviews(122)</div>
+                </div>
+                <div className="py-3 border">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aliquam excepturi fugiat officiis sequi laborum repellat minima et perferendis. Nesciunt quas cupiditate voluptatum neque doloremque fugiat ullam maiores animi sunt dignissimos? Commodi illum, fugiat eligendi, dolor facere necessitatibus reiciendis aut pariatur ipsa voluptatum tempore! Molestias inventore consequatur dolor quo quia.
+                </div>
+            </div>
+        </>
+    )
+}
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom"
 import all_product from "../componants/assets/all_product"
 import BreadCrum from "../componants/breadCrum/BreadCrum"
 import { ProductDisplay } from "../componants/productDisplay/ProductDisplay"
+import { DescriptionBox } from "../componants/descriptionBox/DescriptionBox"
 
 export const Product = () => {
 
@@ -12,6 +13,7 @@ export const Product = () => {
       <div className="mx-28">
         <BreadCrum product={product} />
         <ProductDisplay product={product} />
+        <DescriptionBox/>
       </div>
     </>
   )
